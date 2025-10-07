ae2ffd38e09444d2c0e391a2d7773927bd5bb603
Naila <zahwanaila27@gmail.com>
2025-10-07 08:32:28 -0700

    Create Basic-1-StudyCase5.go

---
+++ Basic-1-StudyCase5.go
@@ -0,0 +1,14 @@
+// Sebuah rumah pintar memiliki beberapa perangkat listrik seperti
+// lampu, AC, dan kulkas. Sistem harus memantau penggunaan daya
+// setiap perangkat setiap jam. Jika penggunaan daya terlalu tinggi,
+// sistem memberikan peringatan dan mencoba menstabilkan konsumsi dengan
+// mengatur ulang prioritas perangkat. Pada akhir hari, sistem menampilkan
+// laporan total energi yang digunakan.
+
+package main
+
+import "fmt"
+
+func main () {
+  
+}
