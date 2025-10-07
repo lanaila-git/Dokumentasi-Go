6595913f02115d91c0e2a9ce17495157c91ca4dc
Naila <zahwanaila27@gmail.com>
2025-10-07 08:24:33 -0700

    Create Basic-1-StudyCase2.go

---
+++ Basic-1-StudyCase2.go
@@ -0,0 +1,12 @@
+// Sebuah aplikasi tiket bus ingin membantu penumpang memilih tujuan perjalanan,
+// waktu keberangkatan, dan tipe kursi. Harga tiket berbeda tergantung rute dan
+// waktu keberangkatan. Penumpang juga bisa mendapatkan promo tertentu jika
+// memenuhi syarat tertentu. Setelah semua pilihan dibuat, sistem menampilkan
+// total harga dan status tiket.
+package main
+
+import "fmt"
+
+func main () {
+  
+}
