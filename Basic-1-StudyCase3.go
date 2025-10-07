aa167553abb6c377fd39dbb7bf9844760992b9d3
Naila <zahwanaila27@gmail.com>
2025-10-07 08:27:35 -0700

    Create Basic-1-StudyCase3.go

---
+++ Basic-1-StudyCase3.go
@@ -0,0 +1,12 @@
+// Bayangkan kamu membuat sistem yang memantau kondisi cuaca harian
+// selama seminggu. Setiap hari, sistem mencatat suhu, curah hujan,
+// dan status cuaca (cerah, mendung, hujan). Di akhir minggu, sistem
+// akan menunjukkan tren cuaca: apakah minggu ini cenderung panas, sejuk, atau hujan terus.
+
+package main
+
+import "fmt"
+
+func main () {
+  
+}
