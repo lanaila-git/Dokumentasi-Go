b3d9f6865513fdf8cc6fe97b1b8c05cb9f9b9f38
Naila <zahwanaila27@gmail.com>
2026-01-13 09:15:55 -0800

    Update Basic-1-StudyCase1.go

--- Basic-1-StudyCase1.go
+++ Basic-1-StudyCase1.go
@@ -3,6 +3,23 @@
 // harga berdasarkan total belanja atau jenis barang tertentu. Setelah transaksi selesai,
 // kasir perlu menampilkan ringkasan pembelian beserta keterangan
 // apakah pelanggan mendapatkan diskon atau tidak.
+
+// rule
+// variabel biasa X apabila diinisiasi sebuah nilai, ketika variabel di print tanpa '&' maka akan menghasilkan nilainya
+// var X int
+// cth: X = 7
+// fmt.Println(X) --> 7
+// namun ketika di print dengan '&' akan menghasilkan alamat memorinya
+// fmt.Println(&X) --> 0x0000##
+// variabel pointer Y apabila diinisiasi(menunjuk) sebuah nilai atau variabel biasa X, 
+// ketika variabel di print tanpa '*' maka akan menghasilkan alamat memori dari X.
+// cth: var Y *int
+// *Y = X
+// fmt.Println(Y) --> 0x0000##
+// namun ketika di print dengan '*' akan menghasilkan nilai X
+// fmt.Println(*Y) --> 7
+// ini berarti Y menunjuk variabel X sehingga mampu mengakses nilainya
+
 package main
 
 import "fmt"
@@ -27,12 +44,12 @@ func main () {
     fmt.Print("Jumlah: ")
     fmt.Scan(&produk.jumlah)
     fmt.Print("Jenis: ")
-    fmt.Scan(&produk.jenis)
+    fmt.Scan(&produk.jenis) //hanya jika "Promo" yang akan medapatkan diskon
     sistem(&produk.harga, &produk.jumlah, &produk.jenis, &produk.nama)
   }
 }
 
-func sistem(*h, *sum, *jns, *nm) {
+func sistem(*h float64, *sum int, jns, nm *string) {
   var calculation float64
   if sum > 2 && jenis != "Promo"{
     calculation = discCountBySum(h, sum, jns)
@@ -57,11 +74,11 @@ func sistem(*h, *sum, *jns, *nm) {
   }
 }
 
-func normalCountBelanja(harga, jumlah, jenis) float64 {
+func normalCountBelanja(harga float64, jumlah int, jenis string) float64 {
   return harga * jumlah
 }
 
-func dicCountBySum(harga, jumlah, jenis) float64 {
+func dicCountBySum(harga float64, jumlah int, jenis string) float64 {
   var disc, total float64
   if jumlah == 3 {
     total = jumlah * harga
@@ -76,14 +93,14 @@ func dicCountBySum(harga, jumlah, jenis) float64 {
   return total
 }
 
-func dicCountByJns(harga, jumlah, jenis) float64 {
+func dicCountByJns(harga float64, jumlah int, jenis string) float64 {
   var total, disc float64
   total = jumlah * harga
   disc = total * 0.45
   return total - disc
 }
 
-func sumOfShopping(*calcu) {
+func sumOfShopping(calcu *float64) {
   var total float64
   total = total + calcu
   fmt.Println("Total belanja anda kali ini adalah")
