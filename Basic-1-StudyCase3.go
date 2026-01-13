c357d224481166c4cdd8848c5da8c036dc01eeb3
Naila <zahwanaila27@gmail.com>
2026-01-13 10:45:13 -0800

    Update Basic-1-StudyCase3.go

--- Basic-1-StudyCase3.go
+++ Basic-1-StudyCase3.go
@@ -8,5 +8,26 @@ package main
 import "fmt"
 
 func main () {
-  
+
+  for i = 1; i <=7; i++ {
+    fmt.Println("Bagaimana cuaca hari ini: ")
+    fmt.Scan(&cuaca)
+    if cuaca == "cerah" {
+      crh = crh + 1
+    } else if cuaca == "mendung" {
+      mdg = mdg + 1
+    } else if cuaca == "hujan" {
+      hjn = hjn + 1
+    }
+  }
+  if crh > mdg && crh > hjn {
+    fmt.Println("predikasi cuaca akan cenderung panas")
+  } else if mdg > crh && mdg > hjn {
+    fmt.Println("predikasi cuaca akan cenderung sejuk")
+  } else if hjn > mdg && hjn > crh {
+    fmt.Println("predikasi cuaca akan cenderung hujan")
+  } else {
+    fmt.Println("tren cuaca tidak dapat di prediksi")
+  }
 }
+
